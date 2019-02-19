971acb3271ad2599a3fb03b656c07bc8c5e960f0
Vic <amerror@gmail.com>
2019-02-18 16:14:08 -0800

    did more things

---
+++ main.js
@@ -0,0 +1,51 @@
+let hackerPic;
+let hackers = []; 
+let numberOfHackers = 1000;
+let whichHacker = 0;
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  hackerPic = loadImage('assets/hacker.png'); // Load the image
+  for (let i = 0; i < numberOfHackers; i++){
+  	hackers[i] = new Hacker();
+  }
+
+}
+
+function draw() {
+	background(0);
+
+	for (let i = 0; i < numberOfHackers; i++){
+		if (hackers[i].visible){
+  		hackers[i].display();
+  		hackers[i].move();
+  	}
+	}
+}
+
+function keyPressed(){
+
+	hackers[whichHacker].visible = true;
+	whichHacker++;
+}
+
+
+class Hacker {
+	constructor(){
+		this.x = width/2;
+		this.y = height/2;
+		this.smallness = random(200)+50;
+		this.xSpeed = random(-2, 2);
+		this.ySpeed = random(-2, 2);
+		this.visible = false;
+	}	
+
+	display(){
+		image(hackerPic, this.x, this.y, this.smallness, this.smallness);
+	}
+	move(){
+		this.x = this.x + this.xSpeed;
+		this.y = this.y + this.ySpeed;
+	}
+}
